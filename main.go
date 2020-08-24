19ffed6c2cd228ad81c1d6c9d92a129701c6632f
h.nakano <h.nakano@variosecure.net>
2020-08-24 16:02:09 +0900

    うまくいったバージョン

--- main.go
+++ main.go
@@ -3,15 +3,26 @@ package main
 import (
 	"fmt"
 	"net"
+	"strings"
 )
 
 func main() {
 	var remoteaddr = "192.168.1.100:80"
 	var configaddr = "192.168.1.1/24"
 
-	_, cnet, _ := net.ParseCIDR(configaddr)
-	fmt.Printf("cnet: %s\n", cnet.Network())
-	rip := net.ParseIP(remoteaddr)
+	fmt.Printf("remoteaddr: %s\n", remoteaddr)
+	fmt.Printf("configaddr: %s\n", configaddr)
+
+	addrslice := strings.Split(remoteaddr, ":")
+	fmt.Printf("addr: %s\n", addrslice[0])
+	cip, cnet, err := net.ParseCIDR(configaddr)
+	if err != nil {
+		fmt.Printf("error: %s\n", err)
+		return
+	}
+	fmt.Printf("cip: %s\n", cip.String())
+	fmt.Printf("cnet: %s\n", cnet.String())
+	rip := net.ParseIP(addrslice[0])
 	fmt.Printf("raddr: %s\n", rip.String())
 	if cnet.Contains(rip) {
 		fmt.Printf("rip contained cnet.\n")
